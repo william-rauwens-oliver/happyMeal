e9fd96ce65aa5f04e61efb4b33ee409cad439770
Justine Saint-Martin <justine.saint-martin@laplateforme.io>
2024-04-08 21:15:01 +0200

    create autocomplétion search bar

--- js/autocompletion.js
+++ js/autocompletion.js
@@ -1,22 +1,21 @@
-// Fonction pour charger les recettes et filtrer les résultats basés sur l'entrée de l'utilisateur
+
 function searchRecipe(inputText) {
     const input = inputText.toLowerCase();
     const autocompleteList = document.getElementById('autocomplete-list');
-    autocompleteList.innerHTML = ''; // Vider les résultats précédents
+    autocompleteList.innerHTML = ''; 
     if (input.length < 1) {
-        return; // Sortir de la fonction si moins de 2 caractères sont saisis
+        return; 
     }
     fetch('../assets/json/data.json')
         .then(response => response.json())
         .then(data => {
             data.recettes.forEach(recipe => {
-                // Vérifier si le nom de la recette ou un ingrédient correspond à l'entrée de l'utilisateur
                 if (recipe.nom.toLowerCase().includes(input) || recipe.ingredients.some(ingredient => ingredient.nom.toLowerCase().includes(input))) {
                     const div = document.createElement('div');
                     div.textContent = recipe.nom;
                     div.addEventListener('click', function() {
-                        document.getElementById('search-input').value = this.textContent; // Sélectionner la recette cliquée
-                        autocompleteList.innerHTML = ''; // Effacer la liste
+                        document.getElementById('search-input').value = this.textContent; 
+                        autocompleteList.innerHTML = ''; 
                     });
                     autocompleteList.appendChild(div);
                 }
@@ -25,7 +24,6 @@ function searchRecipe(inputText) {
         .catch(error => console.error('Erreur lors du chargement des recettes:', error));
 }
 
-// Vous pourriez vouloir désactiver l'envoi du formulaire lors de l'appui sur Entrée
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 });
