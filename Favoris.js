3595e1b76f43688785f864c9ba6966b2dca2b72f
william-rauwens-oiver <william.rauwens-oliver@laplateforme.io>
2024-04-10 15:27:29 +0200

    add code

--- Favoris.js
+++ Favoris.js
@@ -1,36 +1,52 @@
-function chargerFavoris() {
-  fetch('assets/json/favoris.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.recettes);
+async function chargerFavoris() {
+  try {
+      const response = await fetch('assets/json/favoris.json');
+      if (!response.ok) {
+          throw new Error('Impossible de charger les recettes favorites.');
+      }
+      const data = await response.json();
+      if (!data || !data.recettes || data.recettes.length === 0) {
+          throw new Error('Aucune recette favorite trouvée dans le fichier JSON.');
+      }
       afficherFavoris(data.recettes);
-    })
-    .catch(error => console.error('Erreur de chargement des favoris:', error));
+  } catch (error) {
+      console.error(error.message);
+  }
 }
 
-function afficherFavoris(favoris) {
+function afficherFavoris(recettes) {
   const favorisDiv = document.getElementById('favoris');
-  favoris.forEach(favori => {
-    const favoriCard = `
-      <div class="col-md-4 mb-3">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${favori.nom}</h5>
-            <p class="card-text">${favori.categorie}</p>
-            <p class="card-text">Temps de préparation: ${favori.temps_preparation}</p>
-            <ul class="list-group">
-              ${favori.ingredients.map(ingredient => `<li class="list-group-item">${ingredient.quantite} ${ingredient.nom}</li>`).join('')}
-            </ul>
-            <h6 class="mt-3">Étapes:</h6>
-            <ol>
-              ${favori.etapes.map(etape => `<li>${etape}</li>`).join('')}
-            </ol>
+  recettes.forEach(recette => {
+      const favoriCard = `
+          <div id="${recette.nom}" class="col-md-4 mb-3">
+              <div class="card">
+                  <div class="card-body">
+                      <h5 class="card-title">${recette.nom}</h5>
+                      <p class="card-text">${recette.categorie}</p>
+                      <p class="card-text">Temps de préparation: ${recette.temps_preparation}</p>
+                      <ul class="list-group">
+                          ${recette.ingredients.map(ingredient => `<li class="list-group-item">${ingredient.quantite} ${ingredient.nom}</li>`).join('')}
+                      </ul>
+                      <h6 class="mt-3">Étapes:</h6>
+                      <ol>
+                          ${recette.etapes.map(etape => `<li>${etape}</li>`).join('')}
+                      </ol>
+                      <button class="btn btn-danger" onclick="supprimerRecette('${recette.nom}')">Supprimer</button>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
-    `;
-    favorisDiv.innerHTML += favoriCard;
+      `;
+      favorisDiv.innerHTML += favoriCard;
   });
+}
+
+function supprimerRecette(nom) {
+  const favoriDiv = document.getElementById(nom);
+  if (favoriDiv) {
+      favoriDiv.remove();
+  } else {
+      console.error('La recette à supprimer n\'a pas été trouvée dans le DOM.');
+  }
 }
 
 window.onload = chargerFavoris;
